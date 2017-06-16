c27aae7fa65f66b1a9e9789a39e015450e215878
ReonaldHKT <reonaldhkt+webs@gmail.com>
2017-06-17 07:08:20 +0900

    修正2
    デスクトップ時のコンテンツの大きさを大きく + いろいろ

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+function convertAbsUrl(relativePath){
+  var anchor = document.createElement("a");
+  anchor.href = relativePath;
+  return anchor.href;
+}
+
 console.log("JavaScriptが読み込まれました!");
 
 document.getElementById("pageLinkBox1").onclick = function(){
     location.href = "/omoro_button/index.html";
+    location.href = convertAbsUrl("/omoro_button/index.html");
 }
 
 /*iOS10のSafariで複数指で拡大できてしまうのを防ぐ*/
