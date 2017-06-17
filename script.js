9ec27c5d1af8a54c0da3d381bfb78f4e9094b998
ReonaldHKT <reonaldhkt+webs@gmail.com>
2017-06-17 21:26:26 +0900

    修正4
    ontouchstartで反応するように

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-function convertAbsUrl(relativePath){
-  var anchor = document.createElement("a");
-  anchor.href = relativePath;
-  return anchor.href;
-}
-
-console.log("JavaScriptが読み込まれました!");
-
 document.getElementById("pageLinkBox1").onclick = function(){
     location.href = "https://reonaldhkt.github.io/omoro_button/index.html";
-    //location.href = convertAbsUrl("/omoro_button/index.html");
 }
-
+document.getElementById("pageLinkBox1").ontouchstart = function(){
+    location.href = "https://reonaldhkt.github.io/omoro_button/index.html";
+}
 /*iOS10のSafariで複数指で拡大できてしまうのを防ぐ*/
 /*この文がプログラムの最後にくるようにする*/
 document.getElementById("foundation").addEventListener('touchend', event => {
